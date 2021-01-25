3f01a5a841c6d6deac442fcae9cf52293824d057
vinipx <vinipeixotofagundes@gmail.com>
2021-01-25 23:02:58 +0100

    poolnet - attestations_test.go - typo on index ref

--- x/bridge/keeper/attestations_test.go
+++ x/bridge/keeper/attestations_test.go
@@ -83,8 +83,8 @@ func TestAttestClaim(t *testing.T) {
 			require.NoError(t, err)
 
 			// operators
-			for _, indx := range test.createOperatorsFromAccountIds {
-				account := accounts[indx]
+			for _, index := range test.createOperatorsFromAccountIds {
+				account := accounts[index]
 				consensusAddress := sharedTypes.ConsensusAddress(account)
 				pk := randConsensusKey(t)
 
@@ -99,8 +99,8 @@ func TestAttestClaim(t *testing.T) {
 			app.StakingKeeper.ApplyAndReturnValidatorSetUpdates(ctx)
 
 			// attest claim
-			for _, indx := range test.attestClaimAccountIdx {
-				account := accounts[indx]
+			for _, index := range test.attestClaimAccountIdx {
+				account := accounts[index]
 				consensusAddress := sharedTypes.ConsensusAddress(account)
 				operator, found, err := app.PoolsKeeper.GetOperator(ctx, consensusAddress)
 				require.NoError(t, err)
